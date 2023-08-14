72c19ac627ef65c828560d2e0a679e73f4fdf132
Marc Daya <marc@sinistral.net>
2023-08-14 16:24:22 -0600

    Initial, utility code for rendering world grid and cells.

---
+++ cgl.py
@@ -0,0 +1,44 @@
+
+import tkinter
+
+ORIGIN_X = 10
+ORIGIN_Y = 10
+
+WORLD_CELLS_X = 10
+WORLD_CELLS_Y = 10
+
+CELL_W = 10
+CELL_H = 10
+CELL_PADDING = 3
+
+root = tkinter.Tk()
+
+root.title("cgl")
+root.geometry("500x500")
+
+canvas = tkinter.Canvas(root, width=500, height=500, bg="white")
+canvas.pack()
+
+min_x = ORIGIN_X
+max_x = ORIGIN_X + WORLD_CELLS_X*(CELL_W+(CELL_PADDING*2))
+min_y = ORIGIN_Y
+max_y = ORIGIN_Y + WORLD_CELLS_Y*(CELL_H+(CELL_PADDING*2))
+
+for horizontal in range(WORLD_CELLS_Y+1):
+    y = horizontal * (CELL_H + (CELL_PADDING*2))
+    canvas.create_line(min_x, ORIGIN_Y+y, max_x, ORIGIN_Y+y, fill="black")
+for vertical in range(WORLD_CELLS_X+1):
+    x = vertical * (CELL_W + (CELL_PADDING*2))
+    canvas.create_line(ORIGIN_X+x, min_y, ORIGIN_X+x, max_y, fill="black")
+
+def cell_rect(cell_x, cell_y):
+    rect_x1 = ORIGIN_X + (cell_x*(CELL_W + (CELL_PADDING*2))) + CELL_PADDING
+    rect_y1 = ORIGIN_Y + (cell_y*(CELL_H + (CELL_PADDING*2))) + CELL_PADDING
+    rect_x2 = ORIGIN_X + (cell_x*(CELL_W + (CELL_PADDING*2))) + CELL_W + CELL_PADDING
+    rect_y2 = ORIGIN_Y + (cell_y*(CELL_H + (CELL_PADDING*2))) + CELL_H + CELL_PADDING
+    return [rect_x1, rect_y1, rect_x2, rect_y2]
+
+for i in range(0, 10):
+    canvas.create_rectangle(cell_rect(i, i), fill="red", width=1)
+
+root.mainloop()
