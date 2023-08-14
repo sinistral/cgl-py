620fa431eec23cf72279f2025e02c2bd462ece8a
Marc Daya <marc@sinistral.net>
2023-08-14 16:58:34 -0600

    Basic animation loop, using the Tk Canvas timer.

--- cgl.py
+++ cgl.py
@@ -1,4 +1,6 @@
 
+import datetime
+import random
 import tkinter
 
 ORIGIN_X = 10
@@ -24,12 +26,15 @@ max_x = ORIGIN_X + WORLD_CELLS_X*(CELL_W+(CELL_PADDING*2))
 min_y = ORIGIN_Y
 max_y = ORIGIN_Y + WORLD_CELLS_Y*(CELL_H+(CELL_PADDING*2))
 
-for horizontal in range(WORLD_CELLS_Y+1):
-    y = horizontal * (CELL_H + (CELL_PADDING*2))
-    canvas.create_line(min_x, ORIGIN_Y+y, max_x, ORIGIN_Y+y, fill="black")
-for vertical in range(WORLD_CELLS_X+1):
-    x = vertical * (CELL_W + (CELL_PADDING*2))
-    canvas.create_line(ORIGIN_X+x, min_y, ORIGIN_X+x, max_y, fill="black")
+def draw_grid():
+    for horizontal in range(WORLD_CELLS_Y+1):
+        y = horizontal * (CELL_H + (CELL_PADDING*2))
+        canvas.create_line(min_x, ORIGIN_Y+y, max_x, ORIGIN_Y+y, fill="black")
+    for vertical in range(WORLD_CELLS_X+1):
+        x = vertical * (CELL_W + (CELL_PADDING*2))
+        canvas.create_line(ORIGIN_X+x, min_y, ORIGIN_X+x, max_y, fill="black")
+
+draw_grid()
 
 def cell_rect(cell_x, cell_y):
     rect_x1 = ORIGIN_X + (cell_x*(CELL_W + (CELL_PADDING*2))) + CELL_PADDING
@@ -38,7 +43,21 @@ def cell_rect(cell_x, cell_y):
     rect_y2 = ORIGIN_Y + (cell_y*(CELL_H + (CELL_PADDING*2))) + CELL_H + CELL_PADDING
     return [rect_x1, rect_y1, rect_x2, rect_y2]
 
-for i in range(0, 10):
-    canvas.create_rectangle(cell_rect(i, i), fill="red", width=1)
+def draw_cells():
+    max_range = int(random.random()*10)
+    for i in range(0, max_range):
+        canvas.create_rectangle(cell_rect(i, i), fill="red", outline="black", width=1)
 
+def render_frame():
+    draw_grid()
+    draw_cells()
+
+render_frame()
+
+def animloop():
+    canvas.delete(tkinter.ALL)
+    render_frame()
+    canvas.after(500, animloop)
+
+canvas.after(500, animloop)
 root.mainloop()
