7ff6eeab6819b75c7a5f25b3e86cd962a9b386b6
Marc Daya <marc@sinistral.net>
2023-08-19 17:19:36 -0600

    Minor optimization to cell dimension calculation.
    Pre-calculate the offsets for the cell rectangle coordinates when the
    Engine is initialized, rather than on every tick.

--- engine.py
+++ engine.py
@@ -31,6 +31,9 @@ class Engine:
         self._cell_padding = cell_padding
         self._tick_period_ms = tick_period_ms
 
+        self._rect_offset_x = self._cell_width + (self._cell_padding*2)
+        self._rect_offset_y = self._cell_height + (self._cell_padding*2)
+
     def _draw_grid(self):
         min_x = self._origin_x
         max_x = self._origin_x + self._world.x_range*(self._cell_width+(self._cell_padding*2))
@@ -38,18 +41,18 @@ class Engine:
         max_y = self._origin_y + self._world.y_range*(self._cell_height+(self._cell_padding*2))
 
         for horizontal in range(self._world.y_range+1):
-            y = horizontal * (self._cell_height + (self._cell_padding*2))
+            y = horizontal * self._rect_offset_y
             self._canvas.create_line(min_x, self._origin_y+y, max_x, self._origin_y+y, fill=self._grid_colour)
 
         for vertical in range(self._world.x_range+1):
-            x = vertical * (self._cell_width + (self._cell_padding*2))
+            x = vertical * self._rect_offset_x
             self._canvas.create_line(self._origin_x+x, min_y, self._origin_x+x, max_y, fill=self._grid_colour)
 
     def _cell_rect(self, cell_x, cell_y):
-        rect_x1 = self._origin_x + (cell_x*(self._cell_width + (self._cell_padding*2))) + self._cell_padding
-        rect_y1 = self._origin_y + (cell_y*(self._cell_height + (self._cell_padding*2))) + self._cell_padding
-        rect_x2 = self._origin_x + (cell_x*(self._cell_width + (self._cell_padding*2))) + self._cell_width + self._cell_padding
-        rect_y2 = self._origin_y + (cell_y*(self._cell_height + (self._cell_padding*2))) + self._cell_height + self._cell_padding
+        rect_x1 = self._origin_x + (cell_x*self._rect_offset_x) + self._cell_padding
+        rect_y1 = self._origin_y + (cell_y*self._rect_offset_y) + self._cell_padding
+        rect_x2 = self._origin_x + (cell_x*self._rect_offset_x) + self._cell_width + self._cell_padding
+        rect_y2 = self._origin_y + (cell_y*self._rect_offset_y) + self._cell_height + self._cell_padding
         return [rect_x1, rect_y1, rect_x2, rect_y2]
 
     def _draw_cells(self):
